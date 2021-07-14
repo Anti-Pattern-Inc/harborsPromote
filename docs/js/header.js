9e11b122ac1797bec8880a4b5753571941ee16b6
Makiiiiii <makiko-happy1231@ezweb.ne.jp>
2021-07-14 17:53:33 +0900

    #354 ヘッダーに吹き出しを追加&微調整

--- docs/js/header.js
+++ docs/js/header.js
@@ -1,5 +1,45 @@
 $(function () {
+  $('.header-disabled').hover(
+    function () {
+      $('.transparent-area').remove();
+      if ($('.hovered')) {
+        closeSpeechBubble()
+      }
+      var speechBubble = $(this).next();
+      $(this).addClass('hovered');
+      speechBubble.addClass('hovered');
+      transparentAreaWidth = speechBubble.outerWidth();
+      transparentAreaHeight = 35;
+      $(this).before('<div class="transparent-area hovered"></div>');
+      $('.transparent-area').offset({
+        top: speechBubble.offset().top - 35,
+        left: speechBubble.offset().left
+      })
+      $('.transparent-area').width(transparentAreaWidth);
+      $('.transparent-area').height(transparentAreaHeight);
+    },
+    function () {
+      if (($('.transparent-area').is(':hover'))) {
+        $('.speech-bubble.hovered').mouseleave(
+          function () {
+            $('.transparent-area').remove();
+            closeSpeechBubble();
+          }
+        )
+      } else {
+        $('.transparent-area').remove();
+        closeSpeechBubble();
+      }
+    }
+  )
 
+  function closeSpeechBubble() {
+    $('.header-disabled').removeClass('hovered');
+    // $('.footer-disabled').removeClass('hovered');
+    $('.speech-bubble').removeClass('hovered');
+    // $('.footer-speech-bubble').removeClass('hovered');
+  }
+      
   // ハンバーガー
   $('.nav-toggle').click(function () {
     var deviceWidth = $('html').width();
